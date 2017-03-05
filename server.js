1ca616942f9828bbbc3630694d626e99427248be
Nicholas Delpino <NBDelpino4@gmail.com>
2017-03-05 12:28:05 -0500

    set up server - status: success

--- server.js
+++ server.js
@@ -2,3 +2,26 @@
 
 
 
+
+// require the dependencies
+var express = require('express');
+
+// set up port
+var PORT = process.env.PORT || 8080;
+
+// instantiate express
+var app = express();
+
+// set up express router
+var router = express.Router();
+
+// designate the public folder the the static directory
+app.use(express.static(__dirname + '/public'));
+
+// have every request go through router middleware
+app.use(router);
+
+// listen on the port
+app.listen(PORT, function(){
+    console.log('listening on port:' + PORT);
+});
