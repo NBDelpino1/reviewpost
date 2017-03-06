ed6c9017f3b146141838cae21dafcb2c4ba0771c
Nicholas Delpino <NBDelpino4@gmail.com>
2017-03-05 20:39:30 -0500

    added module exports so I can include my routes in my server file ; created a home and saved route to render the home and saved templates ; view engine already set to handlebars so it knows to look for the file name with .handlebars afterwards

--- config/routes.js
+++ config/routes.js
@@ -1,1 +1,16 @@
 // routes will live here
+
+module.exports = function(router) {
+
+    // leads to homepage
+    router.get('/', function(req, res) {
+        res.render('home');
+    });
+
+
+    // leads to saved.handlebars
+    router.get('/saved', function(req, res) {
+        res.render('saved');
+    });
+
+}
