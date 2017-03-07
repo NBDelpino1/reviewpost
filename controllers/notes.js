5e774006d8ee5300ae59f3ba2aec6e826050e6f4
Nicholas Delpino <NBDelpino4@gmail.com>
2017-03-06 21:13:24 -0500

    created get and save functionality for Note controller

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,1 +1,58 @@
 // will contain CRUD functionality
+
+// bring in Note.js script and date.js scripts
+var Note = require('../scripts/Note');
+var makeDate = require('../scripts/date');
+
+
+module.exports = {
+
+    // (1) *** GET function that will find all of the notes associated with id in question
+
+    get: function(data,cb) {
+
+        Note.find({
+
+            _headlineID: data._id
+
+        }, cb);
+
+    },
+
+    // (2) *** SAVE function will take in data from the user and the call back function
+
+    save: function (data, db) {
+
+        // create an object that will have the headline id associated with the note that is being created
+        // plus the date from the makeDate function that was brought in with the module.export
+        //  plus the noteText which is what the user types in
+        var newNote = {
+
+            _headlineID: data._id,
+            date: makeDate(),
+            noteText: data.noteText
+
+        };
+
+        // create the note and run a function that creates an error or a document
+        // alert if there is an error
+        // otherwise pass the document the new note that was created to the call  back function
+        Note.create(newNote, function (err, doc) {
+
+            if(err) {
+
+                console.log(err);
+
+            }
+            else {
+
+                console.log(doc);
+                cb(doc);
+
+            }
+
+        });
+
+    },
+
+}
