7129f70101b56e7c5f15439fa88481e7d252ed8b
Nicholas Delpino <NBDelpino4@gmail.com>
2017-03-15 21:21:58 -0400

    scrape.js inspection completed; looks good

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -3,60 +3,55 @@
 
 
 // require the two packages (request and cheerio) which will make scrape possible
+// go to whatever website and figure out how to grab the headline and summary
+// create scrape variable that will be exported after; scrape variable will have a callback as the parameter
+
 
 var request = require('request');
 var cheerio = require('cheerio');
 
-// create variable which I can then export at the end
-// make call back
 
 var scrape = function(cb){
 
-    // use request package to request specified website with err, res and body which will have everything I get back
+    // use request package to request specified website,  will get err, res and body back
     request('http://www.nytimes.com', function(err,res,body){
 
-        // assign dollar sign so I can use this kind of like how I use jQuery
+        // so can be used kinda like jQuery
         var $ = cheerio.load(body);
 
-        // empty array that will hold the articles
+        // new empty array that will hold the articles
         var articles = [];
 
-        // magic happens here:
-        // select all the *[theme-summaries]* and on each one of these *[theme-summaries]*....
+        // select all the *[theme-summaries]* and on each one of these *[theme-summaries]* grab the text, cut off any white space at the end for .story-heading and .summary which are both children of theme summary
+        // go through every .theme-summary found on the page until done and store inside the articles array (previously created and empty to start)
+        // once done the call back function will send back articles
 
         $('.theme-summary').each(function(i,element) {
 
-            // grab the text and cut off any white space at the end for *[story-heading and summary]* and store each into a variable
+            // for this .story-heading that it's on right now; grab the child story-heading; grab the text out of that; remove any white sace and store into a variable called head
+            // repeat for .summary
             var head = $(this).children('.story-heading').text().trim();
             var sum = $(this).children('.summary').text().trim();
 
-            // if head and sum exist [meaning if the scaper was able to get the text from both of the the children objects] run rejex method [all it really does is clean up the text with white space]
+            // if head and sum exist it means the scrape was successful so next run a replace rejex method which pretty just cleans up the text with white space
             if(head && sum) {
-
                 var headNeat =  head.replace(/(\r\n|\n|\r\t|\s+)/gm, " ").trim();
-
                 var sumNeat = sum.replace(/(\r\n|\n|\r|\t|\s +)/gm, " ").trim();
 
-                // next make an object out of headNeat and sumNeat and assign it to an attribute that will ne needed to be able to create an article in the model
+                // next make an object out of headNeat and sumNeat and then assign it to the attributes of headline and summary which are required to create an article in the model
                 var dataToAdd = {
                     headline: headNeat,
                     summary: sumNeat
-
                 };
-
                 // push the new dataToAdd into the articles array
                 articles.push(dataToAdd);
             }
-
         });
         console.log(articles)
-        // call back will send me articles that have now filled up the previously created array which was empty before
+        // call back sends back articles
         cb(articles);
-
-
     });
-
 };
 
-// export so I can use this throughout the program
+// export scrape variable to it can be used throughout the program
 module.exports = scrape;
